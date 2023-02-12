fbc0cab60adb05ba90a6d61046542dd88bad262d
dipodidae <senseosoft@gmail.com>
2023-02-12 19:25:35 +0100

    add logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,50 +4,7 @@
   <div>
     <SiteNavigation />
     <slot />
-    <footer class="footer p-10 bg-neutral text-neutral-content">
-      <div>
-        <span class="footer-title">Services</span>
-        <nuxt-link class="link link-hover">
-          Branding
-        </nuxt-link>
-        <nuxt-link class="link link-hover">
-          Design
-        </nuxt-link>
-        <nuxt-link class="link link-hover">
-          Marketing
-        </nuxt-link>
-        <nuxt-link class="link link-hover">
-          Advertisement
-        </nuxt-link>
-      </div>
-      <div>
-        <span class="footer-title">Company</span>
-        <nuxt-link class="link link-hover">
-          About us
-        </nuxt-link>
-        <nuxt-link class="link link-hover">
-          Contact
-        </nuxt-link>
-        <nuxt-link class="link link-hover">
-          Jobs
-        </nuxt-link>
-        <nuxt-link class="link link-hover">
-          Press kit
-        </nuxt-link>
-      </div>
-      <div>
-        <span class="footer-title">Legal</span>
-        <nuxt-link class="link link-hover">
-          Terms of use
-        </nuxt-link>
-        <nuxt-link class="link link-hover">
-          Privacy policy
-        </nuxt-link>
-        <nuxt-link class="link link-hover">
-          Cookie policy
-        </nuxt-link>
-      </div>
-    </footer>
+    <SiteFooter />
   </div>
 </template>
 
